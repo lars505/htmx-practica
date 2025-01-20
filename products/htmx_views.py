b08df6ff387a14e22c29fee7dd571253a75fe844
Angel Lars <lars50593@gmail.com>
2025-01-19 21:12:34 -0600

    🎉 feat: complete practice project with Django and HTMX

--- products/htmx_views.py
+++ products/htmx_views.py
@@ -1,6 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Product
 from django.http import HttpResponse
+
+from django.views.decorators.http import require_http_methods
+from django.views.decorators.csrf import csrf_exempt
 
 def check_product(request):
     product = request.GET.get("product")
@@ -8,6 +11,12 @@ def check_product(request):
     products = Product.objects.filter(name=product)
     
     return render(request,"partials/htmx_components/check_products.html",{'products':products})
+
+
+def validate_price(request):
+    precio = request.GET.get('price')
+
+    return render(request, "partials/htmx_components/validate_price.html",{'price':precio.isnumeric()})
  
 def save_product(request):
     name = request.POST.get("product")
@@ -22,3 +31,12 @@ def save_product(request):
     products = Product.objects.all()
 
     return render(request,"partials/htmx_components/list_all_products.html",{'products':products})
+
+@csrf_exempt
+@require_http_methods(["DELETE"])
+def delete_product(request, id_product):
+    product = get_object_or_404(Product, pk = id_product)
+
+    product.delete()
+
+    return render(request, "partials/htmx_components/list_all_products.html",{'products':Product.objects.all()})
--- products/urls.py
+++ products/urls.py
@@ -6,7 +6,10 @@ urlpatterns =[
 ] 
 htmx_patterns = [
     path('check_product/', htmx_views.check_product, name='check_product'),
+    path('validate_price/', htmx_views.validate_price, name="validate_price"),
+
     path('save_product/', htmx_views.save_product, name='save_product'),
+    path('delete_product/<int:id_product>', htmx_views.delete_product, name='delete_product'),
 ]
 
 urlpatterns += htmx_patterns
